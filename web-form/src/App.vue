7f35659a8ebfc48b517b0b60e33e6c7cc097b97a
chance <116939582+jinchanjeon@users.noreply.github.com>
2024-06-10 18:54:46 +0900

    Add files via upload

---
+++ web-form/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<signupform/>
+</template>
+
+<script>
+import signupform from './components/signupform.vue'
+
+export default {
+  name: 'App',
+  components: {
+    signupform
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin:0;
+  background: #eee;
+}
+</style>
+
