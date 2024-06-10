58e07a84b210c203f1e1f49d05f7c0896d206b3c
chance <116939582+jinchanjeon@users.noreply.github.com>
2024-06-10 18:51:12 +0900

    Add files via upload

---
+++ jobs/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+<h1>homepage</h1>
+<p>lorem</p>
+<p>lorem</p>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
